752d138a6e26d5e867fbbb553875b57d00e7d088
Isadfaisal <faisalisad459@gmail.com>
2024-11-24 22:43:30 +0800

    Update java.js

--- java.js
+++ java.js
@@ -1,3 +1,4 @@
+// availabel at:https://www.w3schools.com/jsreF/event_onloadstart.asp [accessed:21 November 2024]
 window.onload = function () {
     const loader = document.getElementById('loader');
     const content = document.getElementById('content');
