5108a3b105d749fbf5575865ed29ccdeb32a102f
Isadfaisal <faisalisad459@gmail.com>
2024-11-24 21:40:03 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    const loader = document.getElementById('loader');
+    const content = document.getElementById('content');
+    loader.style.opacity = '0';
+    setTimeout(() => {
+        loader.style.display = 'none';
+        content.style.display = 'block';
+    }, 1300);
+};
+
+function redirectToLink1() {
+    window.location.href  = 'https://discord.com/invite/9jesgZvgbt'
+}
+function redirectToLink2() {
+    window.location.href  = 'https://x.com/jojo_wiki'
+}
